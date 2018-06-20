6cd8d2f57ea3d9a87bd0cd7f330ec46fa56960d2
John Maenard Elauria <johnelauria@hotmail.com>
2018-06-20 21:40:54 +0800

    Apply physics engine for running character. Must refactor later

--- core/src/com/twopixeled/blinker/assets/characters/Runner.kt
+++ core/src/com/twopixeled/blinker/assets/characters/Runner.kt
@@ -1,10 +1,18 @@
 package com.twopixeled.blinker.assets.characters
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.g2d.Animation
 import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.graphics.g2d.TextureAtlas
+import com.badlogic.gdx.math.Vector2
+import com.badlogic.gdx.physics.box2d.World
+import com.badlogic.gdx.physics.box2d.Body
+import com.badlogic.gdx.physics.box2d.BodyDef
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer
+import com.badlogic.gdx.physics.box2d.PolygonShape
+import com.badlogic.gdx.physics.box2d.FixtureDef
 import com.twopixeled.blinker.assets.assetTypes.Asset
 import com.twopixeled.blinker.assets.assetTypes.Touchable
 import com.twopixeled.blinker.assets.icons.Teleport
@@ -13,40 +21,52 @@ import com.twopixeled.blinker.assets.icons.Teleport
  * The main character of the game. Will keep on running and teleport when player taps on the screen
  */
 class Runner  : Asset, Touchable {
+    private val showDebug = false
     lateinit var teleport: Teleport
-
-    private var runnerAnimation: Animation<TextureRegion>
+    private lateinit var runnerAnimation: Animation<TextureRegion>
     private var runnerAtlas: TextureAtlas = TextureAtlas(Gdx.files.internal("runner/runner.atlas"))
     private var animationTime = 0f
     private var runnerX = Gdx.graphics.width / 12f
     private var runnerY = 0f
     private var teleporting = false
     private var teleportTimer = 15
+    private lateinit var runnerBody: Body
+    private var world = World(Vector2(0f, -98f), true)
+    private var camera: OrthographicCamera? = null
+    private var debugRenderer: Box2DDebugRenderer? = null
 
     init {
-        runnerAnimation = Animation(0.1f, runnerAtlas.regions)
+        createCamera()
+        createRunner()
+        createFloor()
     }
 
     override fun dispose() {
         runnerAtlas.dispose()
+        world.dispose()
     }
 
     override fun draw(batch: SpriteBatch) {
         animationTime += Gdx.graphics.deltaTime * 1.5f
         val runnerRegion = runnerAnimation.getKeyFrame(animationTime, true)
 
+        runnerX = runnerBody.position.x - runnerRegion.regionWidth * 2
+        runnerY = runnerBody.position.y - runnerRegion.regionHeight * 2
+
         if (teleporting) {
             teleportTime()
         } else {
             batch.draw(runnerRegion, runnerX, runnerY, runnerWidth(runnerRegion), runnerHeight())
-            fallByGravity()
         }
+
+        for (c in 1..4) stepWorld()
+        debugRenderer?.render(world, camera!!.combined)
     }
 
     override fun touchDown(screenX: Float, screenY: Float) {
         if (screenX > Gdx.graphics.width / 1.5f) {
-            runnerX = teleport.teleportX
-            runnerY = teleport.teleportY
+            runnerBody.setTransform(teleport.teleportX, teleport.teleportY, 0f)
+            runnerBody.setLinearVelocity(-teleport.teleportX, Gdx.graphics.height.toFloat())
             teleporting = true
         }
     }
@@ -68,19 +88,57 @@ class Runner  : Asset, Touchable {
         }
     }
 
-    private fun fallByGravity() {
-        if (!teleporting) {
-            if (runnerY > 0) {
-                runnerY -= Gdx.graphics.height / 65f
-            } else {
-                runnerY = 0f
-            }
+    private fun stepWorld() {
+        world.step(1/60f, 6, 2)
+    }
 
-            if (runnerX > Gdx.graphics.width / 12) {
-                runnerX -= Gdx.graphics.width / 600f
-            } else {
-                runnerX = Gdx.graphics.width / 12f
-            }
+    private fun createCamera() {
+        if (showDebug) {
+            debugRenderer = Box2DDebugRenderer()
+            camera = OrthographicCamera(Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
+            camera!!.viewportHeight = Gdx.graphics.height.toFloat()
+            camera!!.viewportWidth = Gdx.graphics.width.toFloat()
+            camera!!.position.set(camera!!.viewportWidth * .5f, camera!!.viewportHeight * .5f, 0f)
+            camera!!.update()
         }
+    }
+
+    private fun createFloor() {
+        val floorShape = PolygonShape()
+        val floorDef = BodyDef()
+        val floorFixtureDef = FixtureDef()
+
+        floorDef.type = BodyDef.BodyType.StaticBody
+
+        val floorBody = world.createBody(floorDef)
+        floorShape.setAsBox(Gdx.graphics.width.toFloat() / 2, 0f)
+
+        floorFixtureDef.shape = floorShape
+        floorFixtureDef.density = 0f
+
+        floorBody.createFixture(floorFixtureDef)
+        floorBody.setTransform(0f, 0f, 0f)
+
+        floorShape.dispose()
+    }
+
+    private fun createRunner() {
+        val runnerBodyDef = BodyDef()
+        val runnerShape = PolygonShape()
+        val runnerFixtureDef = FixtureDef()
+
+        runnerAnimation = Animation(0.1f, runnerAtlas.regions)
+        runnerBodyDef.type = BodyDef.BodyType.DynamicBody
+        runnerBodyDef.position.set(runnerX, runnerY)
+
+        runnerBody = world.createBody(runnerBodyDef)
+        runnerShape.setAsBox(runnerWidth(runnerAnimation.getKeyFrame(0f)) / 2, runnerHeight() / 2)
+
+        runnerFixtureDef.shape = runnerShape
+        runnerFixtureDef.density = 1f
+        runnerFixtureDef.restitution = 0.5f
+
+        runnerBody.createFixture(runnerFixtureDef)
+        runnerShape.dispose()
     }
 }
